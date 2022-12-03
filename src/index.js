d47b88e4360def6c2b5bf0c0d1f9554bc8fab7d9
0zAND1z <0zAND1z.dev@protonmail.com>
2022-12-03 06:51:21 +0530

    Gossipsub working on hard versions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import process from 'node:process'
 import { createLibp2p } from 'libp2p'
 
 // import { tcp } from '@libp2p/tcp'
-import { webSockets } from '@libp2p/websockets'
-import { all } from '@libp2p/websockets/filters'
+import { WebSockets } from '@libp2p/websockets'
+import * as WebSocketsFilters from '@libp2p/websockets/filters'
 
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
@@ -11,60 +11,68 @@ import { multiaddr } from 'multiaddr'
 
 import { bootstrap } from '@libp2p/bootstrap'
 // Known peers addresses
-const bootstrapMultiaddrs = [
-    '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-    '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN'
-]
+// const bootstrapMultiaddrs = [
+//     '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+//     '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN'
+// ]
 
 import { kadDHT } from '@libp2p/kad-dht'
 
-import { floodsub } from '@libp2p/floodsub'
+// import { floodsub } from '@libp2p/floodsub'
+import { GossipSub } from '@chainsafe/libp2p-gossipsub'
+import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
 
-const createNode = async () => {
-    const node = await createLibp2p({
-        addresses: {
-            // add a listen address (localhost) to accept TCP connections on a random port
-            listen: ['/ip4/0.0.0.0/tcp/0/ws']
-        },
-        // transports: [tcp()],
-        transports: [webSockets({
-            filters: all
-        })],
-        connectionEncryption: [noise()],
-        streamMuxers: [mplex()],
-        peerDiscovery: [
-            bootstrap({
-                list: bootstrapMultiaddrs,
-            })
-        ],
-        connectionManager: {
-            autoDial: true,
-        },
-        dht: kadDHT(),
-        pubsub: floodsub()
-    })
-    return node
-}
+
+// const createNode = async () => {
+const node = await createLibp2p({
+    addresses: {
+        // add a listen address (localhost) to accept TCP connections on a random port
+        listen: ['/ip4/0.0.0.0/tcp/0/ws']
+    },
+    // transports: [tcp()],
+    transports: [
+        new WebSockets({
+            filter: WebSocketsFilters.all
+        })
+    ],
+    connectionEncryption: [noise()],
+    streamMuxers: [mplex()],
+    // peerDiscovery: [
+    //     bootstrap({
+    //         list: bootstrapMultiaddrs,
+    //     })
+    // ],
+    connectionManager: {
+        autoDial: true,
+    },
+    // dht: kadDHT(),
+    pubsub: new GossipSub({
+        emitSelf: true
+    }),
+    nat: {
+        enabled: false
+    }
+})
+// return node
+// }
 
 // start libp2p
-const [node] = await Promise.all([
-    createNode()
-])
+// const node = await createNode()
 await node.start()
 console.log('libp2p has started')
 
-// Peer discovery and connection messages from the services
-node.addEventListener('peer:discovery', (evt) => {
-    console.log('Discovered %s', evt.detail.id.toString()) // Log discovered peer
-})
+// // Peer discovery and connection messages from the services
+// node.addEventListener('peer:discovery', (evt) => {
+//     console.log('Discovered %s', evt.detail.id.toString()) // Log discovered peer
+// })
 
-node.connectionManager.addEventListener('peer:connect', (evt) => {
-    console.log('Connected to %s', evt.detail.remotePeer.toString()) // Log connected peer
-})
+// node.connectionManager.addEventListener('peer:connect', (evt) => {
+//     console.log('Connected to %s', evt.detail.remotePeer.toString()) // Log connected peer
+// })
 
-for await (const event of node.dht.findPeer(node.peerId)) {
-    // console.log(event)
-}
+// for await (const event of node.dht.findPeer(node.peerId)) {
+//     // console.log(event)
+// }
 
 import PubSubRoom from 'ipfs-pubsub-room'
 const room = new PubSubRoom(node, 'room-name')
@@ -83,9 +91,10 @@ room.on('subscribed', () => {
 
 console.log("List of peers in the room: ", room.getPeers())
 
-await room.broadcast("Hello!")
-room.on('message', (message) => {
-    console.log("Received a message: ", message)
+room.broadcast('Hello!')
+console.log("Broadcasted message")
+room.once('message', (message) => {
+    console.log("Received a message: ", uint8ArrayToString(message.data))
 })
 
 
